b0ed49abe28172ee4838cb4e28e6eaa0fce24763
jakob <jakob@desktop.local>
2024-12-02 23:43:55 +0100

    bugfixing - not done

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-server_addr = "jelka.fmf.un-lj.si"
+server_addr = "https://jelka.fmf.uni-lj.si"
 
 #docker
 base_url = "unix:///var/run/docker.sock" #location of docker server
 registry_url = "storzi.jakobkralj.com" #location of image regestry
-mem_limits = "1g" #amount of ram that a pattern can consume
+mem_limit = "1g" #amount of ram that a pattern can consume
 load_time = 10 # [s] how long before the current pattern stops should the next pattern load
 
 pipe_location = "/tmp/jelka" #where should the pipe that accepts color data be located
--- main.py
+++ main.py
@@ -1,49 +1,61 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.responses import Response
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from datetime import timedelta, datetime
 import config
 import docker
+import asyncio, httpx
+from typing import Optional
+import time
 
-app = FastAPI()
 engine = create_engine("sqlite:///database.db")
-sesion = Session(engine) 
-client = docker.DockerClient(base_url = config.base_url)
+session = Session(engine) 
+docker_client = docker.DockerClient(base_url = config.base_url)
 registry = config.registry_url
-mounts = [docker.Mount(config.pipe_location, config.pipe_location)]
+mounts = [docker.types.Mount(config.pipe_location, config.pipe_location, type="bind")]
 
+# Variable to store unscheduled direct runs from Jelkob
+interruption = None
 
+# Main class to store patterns for Jelka
 class Pattern(SQLModel, table=True):
     identifier: str = Field(index=True, primary_key=True)
-    name: str
+    name: str # Displayed name of pattern
     description: Optional[str] = None
-    source: str
-    docker: str
+    source: str 
+    docker: str # Name of the docker image to be pulled from repository
     duration: Optional[timedelta] = Field(default=timedelta(seconds=60))
     author: str
     school: Optional[str] = None
-    enabled: bool = True
-    visible: bool = True
-    last_run: Optional[datetime] = Field(default=datetime.fromtimestamp(0))
+    enabled: bool = True # Whether to display the pattern in normal rotation (direct runs are still possible)
+    visible: bool = True # Whether the pattern is displayed on Jelkob
+    changed: Optional[datetime] = Field(default=datetime.fromtimestamp(0)) #When was the pattern last changed (to be re-pulled)
+    last_run: Optional[datetime] = Field(default=datetime.fromtimestamp(0)) #When was the pattern last run (Unix time epoch 0 denotes not being run yet at all)
 
-def create_pattern_from_json(data: Dict[str, Any]) -> Sphere:
-    return Pattern(
-        identifier=data['identifier'],
-        name=data['name'],
-        description=data.get('description', ""),
-        source=data.get('source'),
-        docker=data['docker'],
-        duration=timedelta(seconds=data['duration']),
-        author=data['author'],
-        school=data.get('school', ""),
-        enabled=data.get('enabled', True),
-        visible=data.get('visible', True)
-    )
+def create_pattern_from_json(data):
+    return { 
+            'identifier' : data['identifier'],
+        'name' : data['name'],
+            'description' : data.get('description', ""),
+            'source' : data.get('source'),
+            'docker' : data['docker'],
+            'duration' : timedelta(seconds=data['duration']),
+            'author' : data['author'],
+            'school' : data.get('school', ""),
+            'enabled' : data.get('enabled', True),
+            'visible' : data.get('visible', True),
+            'changed' : datetime.fromisoformat(data.get('changed', datetime.fromtimestamp(0).isoformat())) 
+            }
+# Stores information for the pattern to be run directly
+class Interruption:
+    def __init__ (self, pattern_name):
+        self.pattern_name = pattern_name # Name of pattern to be run
+        self.pattern = sesion.select(Pattern).where(Pattern.identifier == pattern_name).first()
+        self.image = createContainer(self.pattern.docker)
 
-@app.on_event("startup")
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
+# Send Jelkob a ping every 30 seconds, so that Jelkob knows, that Korenine is still alive
 async def send_ping(server_addr: str):
     async with httpx.AsyncClient() as client:
         try:
@@ -51,16 +63,7 @@ async def send_ping(server_addr: str):
         except Exception as e:
             print(f"Ping failed: {e}")
 
-def start_periodic_ping(app: FastAPI, server_addr: str):
-    async def ping_task():
-        while True:
-            await send_ping(server_addr)
-            await asyncio.sleep(30)
-    
-    @app.on_event("startup")
-    async def startup_event():
-        asyncio.create_task(ping_task())
-
+# Every 60 seconds it checks, if any new patterns are available
 async def sync_patterns(server_addr: str):
     async with httpx.AsyncClient() as client:
         try:
@@ -80,18 +83,21 @@ async def sync_patterns(server_addr: str):
 
                 if existing:
                     # Update existing pattern
-                    for key, value in pattern_data.items():
+                    if pattern_data.changed > existing.changed:
+                        docker_client.images.pull(pattern_data["docker"])
+                    for key, value in create_pattern_from_json(pattern_data).items():
                         setattr(existing, key, value)
                 else:
                     # Create new pattern
-                    new_pattern = Pattern(**pattern_data)
+                    print(create_pattern_from_json(pattern_data))
+                    new_pattern = Pattern(**create_pattern_from_json(pattern_data))
                     session.add(new_pattern)
-                    client.images.pull(registry + pattern_data["docker"])
+                    docker_client.images.pull(pattern_data["docker"])
 
             # Remove patterns no longer in remote list
             for pattern in existing_patterns:
                 if pattern.identifier not in remote_identifiers:
-                    client.images.remove(pattern["docker"])
+                    docker_client.images.remove(pattern["docker"])
                     session.delete(pattern)
 
             session.commit()
@@ -106,43 +112,111 @@ def getNextPattern():
         earliest_item.last_run = datetime.now()
         session.add(earliest_item)
         session.commit()
-    return earliest_item
+        return earliest_item, createContainer(earliest_item.docker)
+    else:
+        return None, None
+
 def createContainer( container_name: str):
-    image = client.images.get(container_name)
-    return client.containers.create(image, 
+    image = docker_client.images.get(container_name)
+    return docker_client.containers.create(image, 
                                   mem_limit = config.mem_limit,
                                   mounts = mounts,
                                   network_mode = "host",
                                   detach=True)
 
+async def send_runner_state(server_addr: str, pattern_identifier: str):
+    data = {
+        "pattern": pattern_identifier,
+        "started": datetime.now().isoformat()
+    }
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{server_addr}/runner/state/started", json=data)
+        return response
+
 async def run_pattern():
     current_pattern = None
     current_container = None 
-    next_pattern = getNextPattern()
-    next_container = createContainer()
+    next_pattern, next_container = getNextPattern()
     while True:
         current_pattern, current_container = next_pattern, next_container
         next_pattern, next_container = None, None
+        if current_pattern == None:
+            await asyncio.sleep(60)
+            next_pattern, next_container = getNextPattern()
+            continue
         start_time = time.time_ns()
         current_container.start()
-        while time.time_ns() - start_time < current_pattern["duration"].seconds()*1e9 and current_container.status == "running" :
-            await asynco.sleep(1)
+        send_runner_state(config.server_addr, current_pattern.identifier)
+        while time.time_ns() - start_time < current_pattern["duration"].seconds()*1e9  and not interruption:
+            await asyncio.sleep(1)
             current_container.reload()
+
             if time.time_ns() - start_time + config.load_time*1e9 >= current_pattern["duration"].seconds()*1e9:
-                next_pattern = getNextPatter()
-                next_container = createContainer()
+                next_pattern, next_container = getNextPattern()
+
+            if not current_container.status == "running":
+                next_pattern, next_container = getNextPattern()
+                break
         current_container.kill()
 
-def start_periodic_pattern_sync(app: FastAPI, database_sync: DatabaseSync, server_addr: str):
+        if interruption:
+            next_pattern = interruption.pattern
+            next_image = interruption.image
+            interruption = None
+
+def start_running_patterns(app: FastAPI):
+    app.on_event("startup")
+    async def startup_patterns():
+        asyncio.create_task(run_pattern())
+
+
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     async def pattern_sync_task():
        while True:
-           await database_sync.sync_patterns(server_addr)
-           await asyncio.sleep(60)  # 1 minute
-    
-    app.on_event("startup")
-    async def startup_event():
-       asyncio.create_task(pattern_sync_task())
+           try:
+                await sync_patterns(config.server_addr)
+           except Exception as e:
+                print(f"Ping error: {e}")
 
+           await asyncio.sleep(60)  # 1 minute
+           print("ran sync")
+
+    async def ping_task():
+        while True:
+            try:
+                await send_ping(config.server_addr)
+            except Exception as e:
+                print(e) 
+            await asyncio.sleep(30)
+            print("ran ping")
+    async def run_task():
+        try:
+            await run_pattern()
+        except Exception as e:
+            print(e)
+    SQLModel.metadata.create_all(engine)
+    sync = asyncio.create_task(pattern_sync_task())
+    ping = asyncio.create_task(ping_task())
+    run = asyncio.create_task(run_task())
+
+    yield
+    sync.cancel()
+    ping.cancel()
+    run.cancel()
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/run")
+async def create_interruption(identifier: str):
+    global interruption
+    interruption = Interruption(identifier)
+    return Response(status_code=status.HTTP_200_OK)
 
 if __name__ == "__main__":
     import uvicorn
